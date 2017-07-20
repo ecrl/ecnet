0abb438636d068ff9473018d0b61b950740de4be
TJKessler <travis.j.kessler@gmail.com>
2017-07-20 12:54:30 -0400

    Default driver setup, and example config.

--- ecnet_driver.py
+++ ecnet_driver.py
@@ -9,20 +9,26 @@
 #
 
 import ecnet_server
+import ecnet_data_utils
 
 if __name__ == "__main__":
 
 	server = ecnet_server.Server()			# Creates the Server
 	server.import_data()					# Imports the data to the Server
-	
 	server.create_save_env()				# Creates the folder structure for output files; if not used, a single ANN is saved to the working directory
-	
 	server.create_mlp_model()				# Creates the ANN skeleton based on config variables
 	server.fit_mlp_model_validation()		# Fits the ANN to the data using validation; use "fit_mlp_model()" for non-validation use
 	server.select_best()					# Selects the best trial from each node for each build; must have used "create_save_env()"
 
-	rmse = server.test_model_rmse()			# Returns the RMSE of the ANN tested on the ENTIRE input database
+	rmse = server.test_model_rmse()			# Returns the RMSE of the model(s) tested on the ENTIRE input database
 	print(rmse)
+	
+	r2 = server.test_model_r2()				# Returns the coefficient of determination (r-squared) of the model(s) tested on the ENTIRE input database
+	print(r2)
+	
+	mae = server.test_model_mae()			# Returns the MAE of the model(s) tested on the ENTIRE input database
+	print(mae)
 
-	results = server.use_mlp_model()		# Uses the model on the current test set
-	print(results)
+	results = server.use_mlp_model_all()	# Uses the model on the current data set; use "use_mlp_model()" to grab results just for the test set
+	
+	server.output_results(results, "all", "my_results.csv")			# Outputs results to a specified file; "all" for "use_mlp_model_all()", "test" for "use_mlp_model()"
