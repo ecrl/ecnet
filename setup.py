3bc8e1422db35e9e44bd01e4c1045c257af7cda9
TJKessler <travis.j.kessler@gmail.com>
2017-08-07 00:54:37 -0400

    python setup.py install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'ecnet',
+version = 1.0,
+description = 'UMass Lowell Energy and Combustion Research Laboratory Neural Network Software',
+url = 'http://github.com/tjkessler/ecnet',
+author = 'Travis Kessler',
+author_email = 'Travis_Kessler@student.uml.edu',
+license = 'MIT',
+packages = ['ecnet'],
+install_requires = ["tensorflow","pyyaml"],
+zip_safe = False)
