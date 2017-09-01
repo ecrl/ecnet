061d108d8d3dbb7b63b194cc04ec296cd6f9a492
TJKessler <travis.j.kessler@gmail.com>
2017-09-01 15:08:54 -0400

    update to syntax

--- examples/make_predictions.py
+++ examples/make_predictions.py
@@ -4,24 +4,19 @@ Creating the Server object (for handling project directories, machine learning m
 creating the folder hierarchy for your project (project name, number of build sets, and nodes/set, specified in config),
 importing data from database (specified in config), fitting X machine learning models (where X is number of trials/node,
 specified in config), selecting the best performing trial from each node for the final build set, testing for multiple 
-errors, predicting values for the database and outputting them to an output file.
+errors on your database, predicting values for the database and outputting them to an output file.
 """
-import ecnet
+from ecnet.server import Server
 
-server = ecnet.server.Server()			# Create server object
-server.create_save_env()				# Create a folder structure for your project
-server.import_data()					# Import data
-server.create_mlp_model()				# Create a multilayer perceptron (neural network)
-server.fit_mlp_model_validation()		# Fits the mlp using the input database (with a validation set)
-server.select_best()					# Select best trial from each build node
+sv = Server()							# Create server object
+sv.create_save_env()					# Create a folder structure for your project
+sv.import_data()						# Import data
+sv.create_mlp_model()					# Create a multilayer perceptron (neural network)
+sv.fit_mlp_model_validation()			# Fits the mlp using input database (w/ validation set)
+sv.select_best()						# Select best trial from each build node
 
-rmse = server.test_model_rmse()			# Root-mean-square error
-mae = server.test_model_mae()			# Mean average error
-r2 = server.test_model_r2()				# Coeffecient of determination (r-squared)
+results = sv.use_mlp_model_all()					# Calculate results from data (all sets)
+sv.output_results(results, "all", "cn_results.csv")	# Output results to specified file
 
-results = server.use_mlp_model_all()	# Calculate results from data (all values, not just test set)
-server.output_results(results, "all", "results_output.csv")		# Output results to specified file
-
-print(rmse)
-print(mae)
-print(r2)
+errors = sv.calc_error('rmse','r2','mean_abs_error','med_abs_error')	# Calculates errors for dataset predictions
+print(errors)
