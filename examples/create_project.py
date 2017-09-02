02559c01b50d9545fb5888cc8809b865de532fac
TJKessler <travis.j.kessler@gmail.com>
2017-09-02 16:58:14 -0400

    added example scripts for common ECNet tasks

---
+++ examples/create_project.py
@@ -0,0 +1,20 @@
+"""
+EXAMPLE SCRIPT:
+Project creation and publishing
+
+Creates a machine learning project using Server, creates the project save environment,
+imports the training dataset, creates and fits models, selects the best models for each
+build node, and publishes the project to a '.project' file
+"""
+
+from ecnet.server import Server
+
+sv = Server()					# Create the Server
+sv.create_save_env()			# Creat project save environment
+
+sv.import_data()				# Import data from sv.vars['data_filename']
+sv.create_mlp_model()			# Create a multilayer-perceptron (neural network)
+sv.fit_mlp_model_validation()	# Fits models for each node in each build
+sv.select_best()				# Selects the best performing model for each build's node
+
+sv.publish_project()			# Saves the project environment to a '.project' file
---
+++ examples/limit_db_parameters.py
@@ -0,0 +1,13 @@
+"""
+EXAMPLE SCRIPT:
+Reduces database input parameter dimensionality
+
+Imports a dataset, and reduces the number of input parameters to a specified number based
+on input parameter performance
+"""
+
+from ecnet.server import Server
+
+sv = Server()										# Create the Server
+sv.import_data()									# Imports database
+sv.limit_parameters(15, 'limited_database.csv')		# Limits input dimensionality to 15
