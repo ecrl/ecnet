d250a10a9becec85f75bdadfe2be1ab92abd61db
Travis Kessler <travis.j.kessler@gmail.com>
2017-09-02 20:22:33 -0400

    Update limit_db_parameters.py

--- examples/limit_db_parameters.py
+++ examples/limit_db_parameters.py
@@ -8,6 +8,6 @@ on input parameter performance
 
 from ecnet.server import Server
 
-sv = Server()					                # Create the Server
-sv.import_data()				                # Imports database
+sv = Server()					               # Create the Server
+sv.import_data()				               # Imports database
 sv.limit_parameters(15, 'limited_database.csv')		# Limits input dimensionality to 15
