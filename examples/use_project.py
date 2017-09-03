54c5a208ea11e0a34b8e2407e88d2e9454547a03
Travis Kessler <travis.j.kessler@gmail.com>
2017-09-02 20:27:01 -0400

    Update use_project.py

--- examples/use_project.py
+++ examples/use_project.py
@@ -14,7 +14,7 @@ sv.open_project('cn_v1.0_project')	            # Opens pre-existing project
 sv.import_data('testing_data.csv')				 # Import a new set to test
 
 test_results = sv.use_mlp_model('all')				 # Grab results for whole test set
-sv.output_results(test_results, 'testing_results.csv', 'all')   # Save results to file
+sv.output_results(test_results, 'testing_results.csv', 'all')    # Save results to file
 
 test_errors = sv.calc_error('rmse','r2','mean_abs_error','med_abs_error')	 # Compute errors
 print(test_errors)	                                                         # List errors
