b226f6375719ac4469eb4d6514c9b1a9f1a54560
Travis Kessler <travis.j.kessler@gmail.com>
2017-09-02 20:26:50 -0400

    Update use_project.py

--- examples/use_project.py
+++ examples/use_project.py
@@ -11,11 +11,10 @@ from ecnet.server import Server
 sv = Server()					    # Create the Server
 sv.open_project('cn_v1.0_project')	            # Opens pre-existing project
 
-sv.vars['data_filename'] = ('testing_data.csv')     # Change data filename to a test set
-sv.import_data()				    # Import the test set
+sv.import_data('testing_data.csv')				 # Import a new set to test
 
-test_results = sv.use_mlp_model_all()				# Grab results for whole test set
-sv.output_results(test_results, 'all', 'testing_results.csv')   # Save results to file
+test_results = sv.use_mlp_model('all')				 # Grab results for whole test set
+sv.output_results(test_results, 'testing_results.csv', 'all')   # Save results to file
 
 test_errors = sv.calc_error('rmse','r2','mean_abs_error','med_abs_error')	 # Compute errors
 print(test_errors)	                                                         # List errors
