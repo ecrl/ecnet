b38ca757ef21438c8866e1b288cd9842bd8e90a4
Travis Kessler <travis.j.kessler@gmail.com>
2017-09-02 20:24:54 -0400

    Update use_project.py

--- examples/use_project.py
+++ examples/use_project.py
@@ -1,6 +1,6 @@
 """
 EXAMPLE SCRIPT:
-Using a project to obtain results
+Using a pre-existing project to obtain results
 
 Imports a pre-existing project to the Server environment, imports a testing
 dataset, obtain results and errors for testing dataset
@@ -8,14 +8,14 @@ dataset, obtain results and errors for testing dataset
 
 from ecnet.server import Server
 
-sv = Server()										# Create the Server
-sv.open_project('cn_v1.0_project')					# Opens pre-existing project
+sv = Server()					    # Create the Server
+sv.open_project('cn_v1.0_project')	            # Opens pre-existing project
 
-sv.vars['data_filename'] = ('testing_data.csv')		# Change data filename to a test set
-sv.import_data()									# Import the test set
+sv.vars['data_filename'] = ('testing_data.csv')    # Change data filename to a test set
+sv.import_data()				    # Import the test set
 
 test_results = sv.use_mlp_model_all()				# Grab results for whole test set
-sv.output_results(test_results, 'all', 'testing_results.csv') 		# Save results to file
+sv.output_results(test_results, 'all', 'testing_results.csv')  # Save results to file
 
 test_errors = sv.calc_error('rmse','r2','mean_abs_error','med_abs_error')	# Compute errors
-print(test_errors)		# List errors
+print(test_errors)	                                                        # List errors
