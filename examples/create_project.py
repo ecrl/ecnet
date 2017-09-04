32c1f5ff069bcf66036f423d9b30faf3cecb3678
Travis Kessler <travis.j.kessler@gmail.com>
2017-09-04 01:08:39 -0400

    Update create_project.py

--- examples/create_project.py
+++ examples/create_project.py
@@ -9,11 +9,20 @@ build node, and publishes the project to a '.project' file
 
 from ecnet.server import Server
 
-sv = Server()				         # Create the Server
-sv.create_save_env()			         # Create project save environment
+# Create the Server
+sv = Server()
 
-sv.import_data()			         # Import data from sv.vars['data_filename']
-sv.fit_mlp_model_validation('shuffle_lv') 	 # Fits models for each node in each build, shuffling learn and validate sets
-sv.select_best()			         # Selects the best performing model for each build's node
+# Create project save environment
+sv.create_save_env()
 
-sv.publish_project()			         # Saves the project environment to a '.project' file
+# Import data from sv.vars['data_filename']
+sv.import_data()
+
+# Fits models for each node in each build, shuffling learn and validate sets between trials
+sv.fit_mlp_model_validation('shuffle_lv')
+
+# Selects the best performing model from each build's node
+sv.select_best()
+
+# Saves the project environment to a '.project' file
+sv.publish_project()
