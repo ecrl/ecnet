9e8ac04696d43ce2e4d09d9e870570ceef6eecb9
Hernan Romer <nanug33@gmail.com>
2018-04-18 18:30:23 -0400

    Update abc_script.py

--- examples/abc_script.py
+++ examples/abc_script.py
@@ -7,13 +7,17 @@ Save the scores of each iteration in a text file called scores.txt
 
 from ecnet.abc import ABC
 
+# Define a fitness function
 def fitnessTest(values):
     fit = 0
     for val in values:
         fit+=val
     return fit
-    
+
+# Define a set of value ranges with types attached to them
 values = [('int', (0,100)), ('int', (0,100)), ('int',(0,100)), ('float', (10,1000))]
 
+# Create the abc object
 abc = ABC(fitnessFunction = fitnessTest, amountOfEmployers = 100, valueRanges = values, endValue = 5)
+# Run the colony until the fitness score reaches 5 or less
 abc.runABC()
