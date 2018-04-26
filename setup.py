fc37b5c1e2d2ef2d364407fcd3baf81d740e48bf
Hernan Romer <nanug33@gmail.com>
2018-04-26 17:53:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'ecnet',
-version = "1.3.0.dev2",
+version = "1.3.0.dev1",
 description = 'UMass Lowell Energy and Combustion Research Laboratory Neural Network Software',
 url = 'http://github.com/tjkessler/ecnet',
 author = ['Travis Kessler', 'Hernan Gelaf-Romer'],
