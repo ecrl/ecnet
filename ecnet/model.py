4ebdf30c910649a23e9aab58fcb2501e3906d942
tjkessler <travis.j.kessler@gmail.com>
2018-09-22 13:00:04 -0400

    Update to version number 1.6.0

--- ecnet/model.py
+++ ecnet/model.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # ecnet/error_utils.py
-# v.1.5.3
+# v.1.6.0
 # Developed in 2018 by Travis Kessler <travis.j.kessler@gmail.com>
 #
 # Contains functions necessary creating, training, saving, and reusing neural
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='ecnet',
-    version="1.5.3",
+    version='1.6.0',
     description='UMass Lowell Energy and Combustion Research Laboratory Neural Network Software',
     url='http://github.com/tjkessler/ecnet',
     author='Travis Kessler, Hernan Gelaf-Romer, Sanskriti Sharma',
     author_email='travis.j.kessler@gmail.com, Hernan_Gelafromer@student.uml.edu, Sanskriti_Sharma@student.uml.edu',
     license='MIT',
     packages=['ecnet'],
-    install_requires=["tensorflow", "pyyaml", "numpy", "ecabc", "pygenetics"],
+    install_requires=['tensorflow', 'pyyaml', 'numpy', 'ecabc', 'pygenetics'],
     zip_safe=False
 )
