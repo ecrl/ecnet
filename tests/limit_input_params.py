3b7a192c15956b71d830e224c63eb1e65188602b
Travis Kessler <travis.j.kessler@gmail.com>
2018-12-08 17:36:02 -0500

    Update to filename args; added unit test for no saving

--- tests/limit_input_params.py
+++ tests/limit_input_params.py
@@ -5,7 +5,7 @@ def limit_iterative():
 
     sv = Server()
     sv.import_data('cn_model_v1.0.csv', sort_type='random')
-    sv.limit_input_parameters(3, 'test_limit_1.csv')
+    sv.limit_input_parameters(3, output_filename='test_limit_1.csv')
 
 
 def limit_genetic():
@@ -14,7 +14,7 @@ def limit_genetic():
     sv.import_data('cn_model_v1.0.csv', sort_type='random')
     sv.limit_input_parameters(
         3,
-        'test_limit_2.csv',
+        output_filename='test_limit_2.csv',
         use_genetic=True,
         population_size=3,
         num_generations=2,
@@ -29,7 +29,21 @@ def limit_genetic_mp():
     sv.import_data('cn_model_v1.0.csv', sort_type='random')
     sv.limit_input_parameters(
         3,
-        'test_limit_2.csv',
+        output_filename='test_limit_2.csv',
+        use_genetic=True,
+        population_size=3,
+        num_generations=2,
+        shuffle=True,
+        data_split=[0.65, 0.25, 0.1]
+    )
+
+
+def limit_genetic_no_save():
+
+    sv = Server(num_processes=4)
+    sv.import_data('cn_model_v1.0.csv', sort_type='random')
+    sv.limit_input_parameters(
+        3,
         use_genetic=True,
         population_size=3,
         num_generations=2,
@@ -42,3 +56,4 @@ if __name__ == '__main__':
     limit_iterative()
     limit_genetic()
     limit_genetic_mp()
+    limit_genetic_no_save()
