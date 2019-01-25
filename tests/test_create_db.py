1b6dd246df7e235d6ee54a50e41490756e9960f2
Travis Kessler <travis.j.kessler@gmail.com>
2019-01-24 22:03:19 -0500

    Added unit tests for database creation

---
+++ tests/test_create_db.py
@@ -0,0 +1,28 @@
+from ecnet.tools import create_db
+
+
+def from_names():
+
+    print('Creating database from names...')
+    create_db(
+        'mols_names.txt',
+        'db_from_names.csv',
+        targets='mols_targets.txt'
+    )
+
+
+def from_smiles():
+
+    print('Creating database from SMILES...')
+    create_db(
+        'mols_smiles.txt',
+        'db_from_smiles.csv',
+        targets='mols_targets.txt',
+        form='smiles'
+    )
+
+
+if __name__ == '__main__':
+
+    from_names()
+    from_smiles()
