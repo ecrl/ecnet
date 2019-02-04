cd2fda360c915878d4e2844c4cc6b3a14e211977
Kessler <Travis_Kessler@student.uml.edu>
2019-02-04 16:57:01 -0500

    Added 'Getting Started' tutorial

---
+++ docs/tutorials/Getting Started/scripts/train_select_candidates.py
@@ -0,0 +1,29 @@
+from ecnet import Server
+
+
+def main():
+
+    sv = Server(log_level='debug', num_processes=4)
+    sv.import_data(
+        '../kv_model_v1.0.csv',
+        sort_type='random',
+        data_split=[0.6, 0.2, 0.2]
+    )
+    sv.create_project(
+        '../kinetic_viscosity',
+        num_builds=1,
+        num_nodes=5,
+        num_candidates=25
+    )
+    sv.train_model(
+        validate=True,
+        shuffle='train',
+        data_split=[0.6, 0.2, 0.2]
+    )
+    sv.select_best(dset='test')
+    sv.save_project(clean_up=True)
+
+
+if __name__ == '__main__':
+
+    main()
