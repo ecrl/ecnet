86e715849ed68f365cfd1cd04a33b6ee84f2adac
tjkessler <travis.j.kessler@gmail.com>
2019-03-24 15:36:28 -0400

    Updated tutorial for 3.0.0

--- docs/tutorials/Getting Started/scripts/limit_input_descriptors.py
+++ docs/tutorials/Getting Started/scripts/limit_input_descriptors.py
@@ -1,23 +1,13 @@
 from ecnet import Server
+from ecnet.utils.logging import logger
 
 
 def main():
 
-    sv = Server(log_level='debug', num_processes=4)
-    sv.import_data(
-        '../kv_model_v1.0_full.csv',
-        sort_type='random',
-        data_split=[0.6, 0.2, 0.2]
-    )
-    sv.limit_input_parameters(
-        limit_num=15,
-        output_filename='../kv_model_v1.0.csv',
-        use_genetic=True,
-        population_size=30,
-        num_generations=10,
-        mut_rate=0.2,
-        max_mut_amt=1
-    )
+    logger.stream_level = 'debug'
+    sv = Server(num_processes=4)
+    sv.load_data('../kv_model_v1.0_full.csv')
+    sv.limit_inputs(15, output_filename='../kv_model_v1.0.csv')
 
 
 if __name__ == '__main__':
--- docs/tutorials/Getting Started/scripts/use_project.py
+++ docs/tutorials/Getting Started/scripts/use_project.py
@@ -1,23 +1,13 @@
 from ecnet import Server
+from ecnet.utils.logging import logger
 
 
 def main():
 
-    sv = Server(
-        project_file='kinetic_viscosity.prj',
-        log_level='debug'
-    )
-    sv.use_model(
-        dset='test',
-        output_filename='../kv_test_results.csv'
-    )
-    sv.calc_error(
-        'rmse',
-        'mean_abs_error',
-        'med_abs_error',
-        'r2',
-        dset='test'
-    )
+    logger.stream_level = 'debug'
+    sv = Server(prj_file='kinetic_viscosity.prj')
+    sv.use(dset='test', output_filename='../kv_test_results.csv')
+    sv.errors('rmse', 'mean_abs_error', 'med_abs_error', 'r2', dset='test')
 
 
 if __name__ == '__main__':
