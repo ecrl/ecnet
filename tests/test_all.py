ae95a37ba116178947e01b51b62dece6152bb346
tjkessler <travis.j.kessler@gmail.com>
2019-06-06 19:57:53 -0400

    Added unit tests for Server object

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,7 @@
 import unittest
 
+from ecnet.utils.logging import logger
+
 from models.test_mlp import TestMLP
 from tasks.test_limit_inputs import TestLimit
 from tasks.test_remove_outliers import TestRemoveOutliers
@@ -9,8 +11,10 @@ from tools.test_project import TestUseProject
 from utils.test_data_utils import TestDataUtils
 from utils.test_error_utils import TestErrors
 from utils.test_server_utils import TestServerUtils
+from server.test_server import TestServer
 
 
 if __name__ == '__main__':
 
+    logger.stream_level = 'debug'
     unittest.main()
