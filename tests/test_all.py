f6c2160dd37d246458e332454579118e2ab6a9ef
Kessler <Travis_Kessler@student.uml.edu>
2019-06-06 16:00:22 -0400

    Removed conversions from unit tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -4,7 +4,6 @@ from models.test_mlp import TestMLP
 from tasks.test_limit_inputs import TestLimit
 from tasks.test_remove_outliers import TestRemoveOutliers
 from tasks.test_tune_hyperparameters import TestTune
-from tools.test_conversions import TestConversions
 from tools.test_database import TestDatabase
 from tools.test_project import TestUseProject
 from utils.test_data_utils import TestDataUtils
