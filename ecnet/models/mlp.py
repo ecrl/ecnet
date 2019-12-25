f9af76ff83b839050a43c579fff0c8f309b28dc9
Travis Kessler <travis.j.kessler@gmail.com>
2019-12-25 16:56:00 -0500

    ML backend now uses PyTorch instead of Keras/TensorFlow

--- ecnet/models/mlp.py
+++ ecnet/models/mlp.py
@@ -10,51 +10,46 @@
 
 # Stdlib imports
 from re import compile, IGNORECASE
-from os import devnull, environ
-import sys
 
 # 3rd party imports
-from tensorflow import get_default_graph, logging
-from numpy import array
-stderr = sys.stderr
-sys.stderr = open(devnull, 'w')
-from keras.backend import clear_session, reset_uids
-from keras.callbacks import EarlyStopping
-from keras.layers import Dense
-from keras.losses import mean_squared_error
-from keras.metrics import mae
-from keras.models import load_model, Sequential
-from keras.optimizers import Adam
-sys.stderr = stderr
+from numpy import arange, array, asarray
+from numpy.random import choice
+from torch import load, relu, save, sigmoid, stack, tensor
+from torch.nn import Linear, Module, MSELoss
+from torch.optim import Adam
 
 # ECNet imports
 from ecnet.utils.logging import logger
 
-environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-logging.set_verbosity(logging.ERROR)
-
-H5_EXT = compile(r'.*\.h5', flags=IGNORECASE)
+ECNET_EXT = compile(r'.*\.ecnet', flags=IGNORECASE)
 
 
-class MultilayerPerceptron:
+def check_ext(filename: str):
+    '''Ensures the supplied filename has a `.ecnet` extension
 
-    def __init__(self, filename: str='model.h5'):
+    Args:
+        filename (str): supplied filename
+    '''
+
+    if ECNET_EXT.match(filename) is None:
+        raise ValueError('Invalid filename/extension, must be `.ecnet`: {}'
+                         .format(filename))
+
+
+class MultilayerPerceptron(Module):
+
+    def __init__(self, filename: str='model.ecnet'):
         '''MultilayerPerceptron object: fits neural network to supplied inputs
         and targets
 
         Args:
-            filename (str): path to model save location (.h5 extension)
+            filename (str): path to model save location (.ecnet extension)
         '''
 
-        if H5_EXT.match(filename) is None:
-            raise ValueError(
-                'Invalid filename/extension, must be `.h5`: {}'.format(
-                    filename
-                )
-            )
+        super(MultilayerPerceptron, self).__init__()
+        check_ext(filename)
         self._filename = filename
-        clear_session()
-        self._model = Sequential(name=filename.lower().replace('.h5', ''))
+        self._layers = []
 
     def add_layer(self, num_neurons: int, activation: str,
                   input_dim: int=None):
@@ -62,22 +57,39 @@ class MultilayerPerceptron:
 
         Args:
             num_neurons (int): number of neurons for the layer
-            activation (str): activation function for the layer (see Keras
-                activation function documentation)
+            activation (str): activation function for the layer (`sigmoid`,
+                `relu`, `linear`)
             input_dim (int): if not None (input layer), specifies input
                 dimensionality
         '''
 
-        self._model.add(Dense(
-            units=num_neurons,
-            activation=activation,
-            input_shape=(input_dim,)
+        if activation == 'sigmoid' or activation == sigmoid:
+            activation = sigmoid
+        elif activation == 'relu' or activation == relu:
+            activation = relu
+        elif activation == 'linear' or activation is None:
+            activation = None
+        else:
+            raise ValueError('Unknown activation: {}'.format(activation))
+        if input_dim is not None:
+            self._layers.append((
+                input_dim, num_neurons, activation
+            ))
+        else:
+            if len(self._layers) == 0:
+                raise AttributeError('Must include input dimensionality for '
+                                     'first layer')
+            self._layers.append((
+                self._layers[-1][1], num_neurons, activation
+            ))
+        setattr(self, '_l{}'.format(len(self._layers) - 1), Linear(
+            self._layers[-1][0], self._layers[-1][1]
         ))
 
     def fit(self, l_x: array, l_y: array, v_x: array=None, v_y: array=None,
             epochs: int=1500, lr: float=0.001, beta_1: float=0.9,
             beta_2: float=0.999, epsilon: float=0.0000001, decay: float=0.0,
-            v: int=0, batch_size: int=32):
+            v: int=0, batch_size: int=32, patience: int=32) -> list:
         '''Fits neural network to supplied inputs and targets
 
         Args:
@@ -96,48 +108,73 @@ class MultilayerPerceptron:
             decay (float): learning rate decay for Adam optimizer
             v (int): verbose training, `0` for no printing, `1` for printing
             batch_size (int): number of learning samples per batch
+            patience (int): if performing periodic validation, will wait this
+                many epochs for a better validation loss; will terminate
+                training if better loss not found
+
+        Returns:
+            list: list of tuples, (train loss, valid loss) at each epoch; if
+                not performing periodic validation, index 1 of tuple is None
         '''
 
-        self._model.compile(
-            loss=mean_squared_error,
-            optimizer=Adam(
-                lr=lr,
-                beta_1=beta_1,
-                beta_2=beta_2,
-                epsilon=epsilon,
-                decay=decay
-            ),
-            metrics=[mae]
+        criterion = MSELoss()
+        optimizer = Adam(
+            self.parameters(),
+            lr,
+            (beta_1, beta_2),
+            epsilon,
+            decay
         )
 
-        if v_x is not None and v_y is not None:
-            history = self._model.fit(
-                l_x,
-                l_y,
-                validation_data=(v_x, v_y),
-                callbacks=[EarlyStopping(
-                    monitor='val_loss',
-                    patience=250,
-                    verbose=v,
-                    mode='min',
-                    restore_best_weights=True
-                )],
-                epochs=epochs,
-                verbose=v,
-                batch_size=batch_size
-            )
-            epochs = len(history.history['loss'])
-        else:
-            self._model.fit(
-                l_x,
-                l_y,
-                epochs=epochs,
-                verbose=v,
-                batch_size=batch_size
-            )
+        losses = []
+        trained_epochs = epochs
 
-        logger.log('debug', 'Training complete after {} epochs'.format(epochs),
-                   call_loc='MLP')
+        if v_x is not None and v_y is not None:
+            best_model_params = self.state_dict()
+            best_valid_loss = None
+            valid_count = 0
+            for e in range(epochs):
+                train_x, train_y = self._batch(l_x, l_y, batch_size)
+                valid_x, valid_y = self._batch(v_x, v_y, batch_size)
+                optimizer.zero_grad()
+                train_output = self(train_x)
+                train_loss = criterion(train_output, train_y)
+                train_loss.backward()
+                optimizer.step()
+                valid_output = self(valid_x)
+                valid_loss = criterion(valid_output, valid_y)
+                if v == 1:
+                    logger.log('debug', 'Epoch {} | Training Loss {} | '
+                               'Validation Loss {}'.format(e + 1, train_loss,
+                               valid_loss), call_loc='MLP')
+                losses.append((train_loss.item(), valid_loss.item()))
+                if best_valid_loss is None or valid_loss < best_valid_loss:
+                    best_model_params = self.state_dict()
+                    best_valid_loss = valid_loss
+                    valid_count = 0
+                else:
+                    valid_count += 1
+                    if valid_count >= patience:
+                        trained_epochs = e + 1
+                        break
+            self.load_state_dict(best_model_params)
+
+        else:
+            for e in range(epochs):
+                train_x, train_y = self._batch(l_x, l_y, batch_size)
+                optimizer.zero_grad()
+                output = self(train_x)
+                loss = criterion(output, train_y)
+                if v == 1:
+                    logger.log('debug', 'Epoch {} | Training Loss {}'
+                               .format(e + 1, loss), call_loc='MLP')
+                losses.append((loss.item(), None))
+                loss.backward()
+                optimizer.step()
+
+        logger.log('debug', 'Training complete after {} epochs'
+                   .format(trained_epochs), call_loc='MLP')
+        return losses
 
     def use(self, x: array) -> array:
         '''Uses neural network to predict values for supplied data
@@ -149,11 +186,10 @@ class MultilayerPerceptron:
             numpy.array: predictions
         '''
 
-        with get_default_graph().as_default():
-            return self._model.predict(x)
+        return asarray(self(tensor(x).float()).tolist())
 
     def save(self, filename: str=None):
-        '''Saves neural network to .h5 file
+        '''Saves neural network to specified file
 
         filename (str): if None, uses MultilayerPerceptron._filename;
             otherwise, saves to this file
@@ -161,25 +197,73 @@ class MultilayerPerceptron:
 
         if filename is None:
             filename = self._filename
-        if H5_EXT.match(filename) is None:
-            raise ValueError(
-                'Invalid filename/extension, must be `.h5`: {}'.format(
-                    filename
-                )
-            )
-        self._model.save(filename)
+        check_ext(filename)
+        save({
+            'layers': self._layers,
+            'state_dict': self.state_dict()
+        }, filename)
         logger.log('debug', 'Model saved to {}'.format(filename),
                    call_loc='MLP')
 
     def load(self, filename: str=None):
-        '''Loads neural network from .h5 file
+        '''Loads neural network from specified file
 
         Args:
-            filename (str): path to .h5 model file
+            filename (str): if None, uses MultilayerPerceptron._filename;
+                otherwise, loads from this file
         '''
 
         if filename is None:
             filename = self._filename
-        self._model = load_model(filename)
+        check_ext(filename)
+        file_contents = load(filename)
+        layers = file_contents['layers']
+        self._layers = []
+        for idx, l in enumerate(layers):
+            if idx == 0:
+                self.add_layer(l[1], l[2], l[0])
+            else:
+                self.add_layer(l[1], l[2])
+        self.load_state_dict(file_contents['state_dict'])
         logger.log('debug', 'Model loaded from {}'.format(filename),
                    call_loc='MLP')
+
+    def forward(self, x: tensor) -> tensor:
+        '''Used by torch.nn.Module to perform forward pass through neural
+        network
+
+        Args:
+            x (torch.tensor): data to pass through neural network
+
+        Returns:
+            tensor: final layer's output
+        '''
+
+        for idx, layer in enumerate(self._layers):
+            if layer[2] is None:
+                x = getattr(self, '_l{}'.format(idx))(x)
+            else:
+                x = layer[2](getattr(self, '_l{}'.format(idx))(x))
+        return x
+
+    @staticmethod
+    def _batch(x: array, y: array, batch_size: int) -> array:
+        '''Returns a batch with specified size given input/target data
+
+        Args:
+            x (np.array): input data
+            y (np.array): target data, same # samples as input data
+            batch_size (int): size of the batch
+
+        Returns:
+            tuple: (np.array, np.array), each array of size batch_size
+        '''
+
+        if batch_size >= len(x):
+            x_b = tensor(x).float()
+            y_b = tensor(y).float()
+        else:
+            idxs = choice(arange(len(x)), batch_size, replace=False)
+            x_b = tensor(x[idxs]).float()
+            y_b = tensor(y[idxs]).float()
+        return (x_b, y_b)
--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
 from setuptools import setup, find_packages
+from platform import system, mac_ver
+
+install_requires = [
+    'alvadescpy==0.1.0',
+    'colorlogging==0.3.5',
+    'ecabc==2.2.3',
+    'matplotlib==3.1.2',
+    'numpy==1.16.4',
+    'padelpy==0.1.6',
+    'pyyaml==5.1.1',
+    'scikit-learn==0.21.2'
+]
+
+s = system()
+if s == 'Windows':
+    install_requires.append(
+        'torch @ https://download.pytorch.org/whl/cpu/torch-1.3.1%2Bcpu-cp37-cp37m-win_amd64.whl'
+    )
+elif s == 'Linux':
+    if mac_ver()[0] != '':
+        install_requires.append(
+            'torch @ https://download.pytorch.org/whl/cpu/torch-1.3.1-cp37-none-macosx_10_7_x86_64.whl'
+        )
+    else:
+        install_requires.append(
+            'torch @ https://download.pytorch.org/whl/cpu/torch-1.3.1%2Bcpu-cp37-cp37m-linux_x86_64.whl'
+        )
+else:
+    raise OSError('Unsupported OS: {}'.format(s))
 
 setup(
     name='ecnet',
@@ -11,17 +40,7 @@ setup(
                  ' Hunter_Mack@uml.edu',
     license='MIT',
     packages=find_packages(),
-    install_requires=[
-        'alvadescpy==0.1.0',
-        'colorlogging==0.3.5',
-        'ecabc==2.2.3',
-        'keras==2.2.4',
-        'matplotlib==3.1.0',
-        'numpy==1.16.4',
-        'padelpy==0.1.6',
-        'pyyaml==5.1.1',
-        'scikit-learn==0.21.2',
-        'tensorflow==1.13.1'
-    ],
+    python_requires='~=3.7',
+    install_requires=install_requires,
     zip_safe=False
 )
