35bdb5d3fcbddb59e6ea4342edea5b9b48da994f
tjkessler <travis.j.kessler@gmail.com>
2020-01-02 13:39:53 -0500

    Range of patience for tuning: [64, 1024]

--- ecnet/tasks/tuning.py
+++ ecnet/tasks/tuning.py
@@ -76,7 +76,7 @@ def tune_hyperparameters(df: DataFrame, vars: dict, num_employers: int,
         ('float', (1e-9, 1e-4)),    # Learning rate decay
         ('float', (1e-5, 0.1)),     # Learning rate
         ('int', (1, len(df))),      # Batch size
-        ('int', (1, 1600))          # Patience
+        ('int', (64, 1024))         # Patience
     ]
 
     for _ in range(len(vars['hidden_layers'])):
