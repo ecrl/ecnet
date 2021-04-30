740f4309c86280da7b50b89e809fa23a7f3bb48b
Travis Kessler <travis.j.kessler@gmail.com>
2021-04-30 16:37:06 -0400

    Updates to tuning docstrings, arguments

--- ecnet/tasks/__init__.py
+++ ecnet/tasks/__init__.py
@@ -1,2 +1,3 @@
 from .feature_selection import select_rfr
-from .parameter_tuning import tune_batch_size, tune_model_architecture, tune_training_parameters
+from .parameter_tuning import N_TESTS, CONFIG, tune_batch_size, tune_model_architecture,\
+    tune_training_parameters
